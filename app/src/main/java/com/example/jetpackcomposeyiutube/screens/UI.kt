e7d30309a6d439385896a8fa06a21108f1964eb1
Umedjon Saidulloev <umedjon.saidulloev@mail.ru>
2024-04-06 16:30:55 +0500

    Lesson 10

--- app/src/main/java/com/example/jetpackcomposeyiutube/screens/UI.kt
+++ app/src/main/java/com/example/jetpackcomposeyiutube/screens/UI.kt
@@ -1,5 +1,6 @@
 package com.example.jetpackcomposeyiutube.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,11 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Text
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,13 +26,29 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.jetpackcomposeyiutube.data.WeatherModel
 import com.example.jetpackcomposeyiutube.ui.theme.BlueLight
+import java.util.Currency
 
 @Composable
-fun ListItem(item: WeatherModel) {
+fun MainList(list: List<WeatherModel>,  currentDay: MutableState<WeatherModel>){
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        itemsIndexed(
+           list
+        ) { _, item ->
+            ListItem(item, currentDay)
+        }
+    }
+}
+@Composable
+fun ListItem(item: WeatherModel, currentDay: MutableState<WeatherModel>) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 5.dp),
+            .padding(top = 5.dp).clickable {
+                if (item.hours.isEmpty()) return@clickable
+                currentDay.value = item
+            },
         colors = CardDefaults.cardColors(BlueLight),
         elevation = CardDefaults.cardElevation(0.dp),
         shape = CardDefaults.shape
